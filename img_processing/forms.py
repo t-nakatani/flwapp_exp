8695a98f0f7211ae0637ff2b91277646ffb4ee23
t-nakatani <tomoya.12@icloud.com>
2022-10-21 00:11:48 +0900

    [ADD] feature questionnaire

--- img_processing/forms.py
+++ img_processing/forms.py
@@ -1,9 +1,18 @@
-from traceback import format_stack
-from img_processing.models import Image
+from img_processing.models import Image, Questionnaire
 from django import forms
 
 
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = {'path', 'img'}
+        fields = {'img_id', 'img'}
+
+
+class QuestionnaireForm(forms.ModelForm):
+    class Meta:
+        model = Questionnaire
+        fields = {'trouble', 'to_be_improved'}
+        labels = {
+            'trouble': '実験中困ったことがあれば教えてください',
+            'to_be_improved': '改善点があれば教えてください',
+        }
