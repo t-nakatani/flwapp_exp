39c898388fb5c536c52ea63d8062a8b370a7ac39
t-nakatani <tomoya.12@icloud.com>
2022-10-21 18:39:42 +0900

    [UPDATE] debug = false

--- flwapp/settings.py
+++ flwapp/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'gunicorn-django']
 CSRF_TRUSTED_ORIGINS = ['https://flwapp.imlab.jp']
