61516527bf96f5a61fcd99b10104842a76f5c324
t-nakatani <tomoya.12@icloud.com>
2022-10-21 21:24:55 +0900

    [UPDATE] media setting for deploy

--- flwapp/settings.py
+++ flwapp/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
-if DEBUG:
-    MEDIA_ROOT = BASE_DIR / 'media'
-else:
-    MEDIA_ROOT = f'/var/www/{BASE_DIR.name}/media'
+# if DEBUG:
+MEDIA_ROOT = BASE_DIR / 'media'
+# else:
+# MEDIA_ROOT = f'/var/www/{BASE_DIR.name}/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
